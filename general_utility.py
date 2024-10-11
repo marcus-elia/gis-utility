ec71a28fd66f8ddaaba54ff0b1f2671ffaa8a983
marcus-elia <mselia@uvm.edu>
2024-10-11 09:13:49 -0400

    Add time and size logging to remove tags except

--- general_utility.py
+++ general_utility.py
@@ -13,9 +13,16 @@ COMMON_ABBREVIATIONS = (\
         ("interstate", "i"),\
         ("lane", "ln"),\
         ("parkway", "pkwy"),\
+        ("place", "pl"),\
         ("street", "st"),\
         ("road", "rd"),\
-        ("terrace", "ter"))
+        ("terrace", "ter"),\
+        ("trail", "tr"),\
+        ("east", "e"),\
+        ("north", "n"),\
+        ("south", "s"),\
+        ("west", "w")
+        )
 
 def standardize_string(s):
     """
@@ -26,3 +33,28 @@ def standardize_string(s):
     for word, abbr in COMMON_ABBREVIATIONS:
         cleaned = cleaned.replace(word, abbr)
     return cleaned
+
+def get_time_estimate_string(time_elapsed, num_complete, num_total):
+    percent_complete = float(num_complete) / float(num_total) * 100
+    time_remaining = int(time_elapsed * (100 - percent_complete) / percent_complete)
+    if time_elapsed < 120:
+        time_string = "%d seconds, " % (time_elapsed)
+    else:
+        time_elapsed = int(time_elapsed / 60)
+        if time_elapsed < 120:
+            time_string = "%d minutes, " % (time_elapsed)
+        else:
+            time_elapsed = int(time_elapsed / 60)
+            time_string = "%d hours, " % (time_elapsed)
+    if time_remaining < 120:
+        time_string += "%d seconds remaining." % (time_remaining)
+    else:
+        time_remaining = int(time_remaining / 60)
+        if time_remaining < 120:
+            time_string += "%d minutes remaining." % (time_remaining)
+        else:
+            time_remaining = int(time_remaining / 60)
+            time_string += "%d hours remaining" % (time_remaining)
+
+    return "Completed %d/%d (%.1f percent) in %s" % (num_complete, num_total, percent_complete, time_string)
+
--- remove_tags_except.py
+++ remove_tags_except.py
@@ -3,31 +3,15 @@
 import argparse
 from enum import Enum
 import geojson
+import os
+import time
 
-from general_utility import standardize_string
+from general_utility import standardize_string, get_time_estimate_string
 
 """
 OSM data often has a lot of non-useful tags that I consider to be clutter.
 This script only retains the ones you specify.
 """
-
-class KeyPresence(Enum):
-    Both = 1
-    Key1Only = 2
-    Key2Only = 3
-    Neither = 4
-
-def check_key_presence(properties, key1, key2):
-    key1_present = key1 in properties and properties[key1]
-    key2_present = key2 in properties and properties[key2]
-    if key1_present and key2_present:
-        return KeyPresence.Both
-    elif key1_present:
-        return KeyPresence.Key1Only
-    elif key2_present:
-        return KeyPresence.Key2Only
-    else:
-        return KeyPresence.Neither
 
 def main():
     parser = argparse.ArgumentParser(description="Remove all attributes except the keys given in the text file.")
@@ -36,10 +20,15 @@ def main():
     parser.add_argument("-o", "--output-geojson-path", required=False, help="Path to output geojson file. If omitted, write to input file.")
 
     args = parser.parse_args()
-    
+
+    print("Loading %s" % (args.input_geojson_path))
+    start_time = time.time()
     f = open(args.input_geojson_path, 'r', encoding="utf8")
     geojson_contents = geojson.loads(f.read())
     f.close()
+    print("Loaded %d features in %.2f seconds" % (len(geojson_contents['features']), time.time() - start_time))
+
+    original_file_size_mb = os.stat(args.input_geojson_path).st_size / (1024 * 1024)
 
     # Get a list of keys to retain from the file.
     keys_to_keep = []
@@ -49,31 +38,55 @@ def main():
     f.close()
 
     # Iterate over every key in every feature to get a set of keys to remove.
+    print("Compiling set of keys to remove")
     keys_to_remove = set()
+    start_time = time.time()
+    num_points = len(geojson_contents['features'])
+    num_completed = 0
     for geojson_feature in geojson_contents['features']:
         for key in geojson_feature.properties:
             if not key in keys_to_keep:
                 keys_to_remove.add(key)
+        # Log the status
+        num_completed += 1
+        time_elapsed = int(time.time() - start_time)
+        print(get_time_estimate_string(time_elapsed, num_completed, num_points), end='\r')
+    print()
 
     # Iterate over every feature and remove the keys we don't want.
     num_attributes_removed = 0
+    num_null_removed = 0
     num_features = 0
+    start_time = time.time()
+    num_points = len(geojson_contents['features'])
+    num_completed = 0
+    print("Removing keys from features")
     for geojson_feature in geojson_contents['features']:
         num_features += 1
         for key in keys_to_remove:
             try:
+                if not geojson_feature.properties[key]:
+                    num_null_removed += 1
                 del geojson_feature.properties[key]
                 num_attributes_removed += 1
             except KeyError:
                 pass
+        # Log the status
+        num_completed += 1
+        time_elapsed = int(time.time() - start_time)
+        print(get_time_estimate_string(time_elapsed, num_completed, num_points), end='\r')
+    print()
 
     output_geojson_path = args.output_geojson_path if args.output_geojson_path else args.input_geojson_path
     f = open(output_geojson_path, 'w', encoding="utf8")
     f.write(geojson.dumps(geojson_contents))
     f.close()
 
+    final_file_size_mb = os.stat(output_geojson_path).st_size / (1024 * 1024)
     average_num_removed = num_attributes_removed / num_features
-    print("Removed %.2f keys per feature for %d features." % (average_num_removed, num_features))
+    percent_null = num_null_removed / num_attributes_removed * 100
+    print("Removed %.2f keys per feature for %d features. %f%% were null." % (average_num_removed, num_features, percent_null))
+    print("File size reduced from %.2f MB to %.2f MB." % (original_file_size_mb, final_file_size_mb))
 
 if __name__ == "__main__":
     main()
