3f08dd24dc5354e2ac737e51253279c8719960f4
marcus-elia <mselia@uvm.edu>
2024-10-24 21:56:58 -0400

    Add more modes from editing tags

--- apply_to_geojsons_recursively.py
+++ apply_to_geojsons_recursively.py
@@ -8,6 +8,7 @@ whose names end with a certain suffix.
 import argparse
 import geojson
 import geopandas
+import json
 import shapely
 import ntpath
 import os
@@ -52,13 +53,40 @@ def reproject_to_epsg(full_path, epsg):
     gdf = gdf.to_crs(int(epsg))
     gdf.to_file(full_path)    
 
+def sanitize_tags(geojson_path, keys_and_values_json_path):
+    orig_size = os.stat(geojson_path).st_size
+    gdf = geopandas.read_file(geojson_path)
+    f = open(keys_and_values_json_path, 'r')
+    general_name_to_specific_name = json.loads(f.read())
+    f.close()
+    gdf = gdf.rename(columns={value: key for key, value in general_name_to_specific_name['keys'].items()})
+    desired_keys = list(general_name_to_specific_name['keys'].keys()) + ['geometry']
+    gdf = gdf[desired_keys]
+    gdf.to_file(geojson_path)
+    print("Reduced size by %.1f%% for %s." % (100 - os.stat(geojson_path).st_size / orig_size * 100, geojson_path))
+
+def harmonize_tag(geojson_path, key, bad_value, good_value):
+    gdf = geopandas.read_file(geojson_path)
+    gdf.loc[gdf[key] == bad_value, key] = good_value
+    gdf.to_file(geojson_path)
+
+def restrict_to_attribute(geojson_path, key, required_value):
+    orig_size = os.stat(geojson_path).st_size
+    gdf = geopandas.read_file(geojson_path)
+    gdf = gdf[gdf[key] == required_value]
+    gdf.to_file(geojson_path)
+    print("Reduced size by %.1f%% for %s." % (100 - os.stat(geojson_path).st_size / orig_size * 100, geojson_path))
+
 def main():
     parser = argparse.ArgumentParser(description="Apply operations to geojson files.")
     parser.add_argument("-d", "--parent-dir", required=True, help="Path to parent directory.")
     parser.add_argument("-s", "--filename-suffix", required=True, help="The target suffix for geojson files. E.g. 'County' -> 'YatesCounty.geojson'.")
     operation_group = parser.add_mutually_exclusive_group(required=True)
     operation_group.add_argument("--add-bbox-file", action='store_true', help='Create an adjacent file containing the bbox of the polygon.')
-    operation_group.add_argument("--reproject-to-epsg", help='Reproject geojson in place to the specified EPSG code.')
+    operation_group.add_argument("--reproject-to-epsg", help='Reproject geojson to the specified EPSG code.')
+    operation_group.add_argument("--sanitize-tags", help='Remove unneeded attributes and clean/standardize the others. Pass JSON filepath that maps "key":{"general_name":"specific_name".')
+    operation_group.add_argument("--harmonize-tag", help='Pass a + delimited string: "key+bad_value+good_value".')
+    operation_group.add_argument("--restrict-to-attribute", help='Pass a + delimited string: "key+required_value".')
 
     args = parser.parse_args()
 
@@ -72,6 +100,14 @@ def main():
                 create_bbox_file(full_path, args.filename_suffix)
             elif args.reproject_to_epsg:
                 reproject_to_epsg(full_path, args.reproject_to_epsg)
+            elif args.sanitize_tags:
+                sanitize_tags(full_path, args.sanitize_tags)
+            elif args.harmonize_tag:
+                key, bad_value, good_value = args.harmonize_tag.split('+')
+                harmonize_tag(full_path, key, bad_value, good_value)
+            elif args.restrict_to_attribute:
+                key, value = args.restrict_to_attribute.split('+')
+                restrict_to_attribute(full_path, key, value)
             # Log the status
             num_completed += 1
             time_elapsed = int(time.time() - start_time)
