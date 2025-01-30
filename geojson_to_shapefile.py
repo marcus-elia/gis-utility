40c5da5946faa6e4f1afeb8568c9540d1dc29d8b
marcus-elia <mselia@uvm.edu>
2025-01-29 19:18:40 -0500

    Script calling geopandas converting geojson to shp

---
+++ geojson_to_shapefile.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import argparse
+import geopandas
+import os
+import zipfile
+
+SHAPEFILE_EXTENSIONS = (".cpg", ".dbf", ".prj", ".shp", ".shx")
+
+def main():
+    parser = argparse.ArgumentParser(description="Convert geojson to shapefile.")
+    parser.add_argument("-i", "--input-geojson-path", required=True, help="Path to input geojson file.")
+    parser.add_argument("-o", "--output-shapefile-path", required=True, help="Path to output shapefile file.")
+    parser.add_argument("--zip", action='store_true', help="If present, move output files into a zip.")
+
+    args = parser.parse_args()
+
+    gdf = geopandas.read_file(args.input_geojson_path)
+    gdf.to_file(args.output_shapefile_path)
+
+    # Note: this is not needed because you can give a .shp.zip filepath to geopandas.to_file().
+    if args.zip:
+        base_filepath = args.output_shapefile_path.split('.')[-1]
+        zip_filepath = args.output_shapefile_path + ".zip"
+        with zipfile.ZipFile(zip_filepath, 'w') as zip_file:
+            for extension in SHAPEFILE_EXTENSIONS:
+                filepath = base_filepath + extension
+                zip_file.write(filepath)
+                os.remove(filepath)
+
+if __name__ == "__main__":
+    main()
