52d18c66eda1e206ced759850d3b2ba0b8eee7e2
marcus-elia <mselia@uvm.edu>
2025-02-15 22:33:45 -0500

    Add subdivide mode

--- query_map_server.py
+++ query_map_server.py
@@ -7,6 +7,7 @@ import time
 from general_utility import get_time_estimate_string
 
 CONVERT_TO_SHAPEFILE_NAME = "geojson_to_shapefile.py"
+THIS_SCRIPT_NAME = "query_map_server.py"
 
 # Configurable variables
 FIELDS = "*"  # Fields to retrieve, '*' for all fields
@@ -29,25 +30,39 @@ def save_geojson(data, file_name):
     with open(file_name, "w") as f:
         json.dump(data, f)
 
+def number_to_command_line_arg(num):
+    """
+    Negative numbers need spaces in front.
+    """
+    if num < 0:
+        return " " + str(num)
+    else:
+        return str(num)
+
+def get_server_info(base_url):
+    info_url = base_url + "?f=json"
+    info_response = requests.get(info_url)
+    info_data = info_response.json()
+    max_record_count = info_data.get("maxRecordCount", "Not specified")
+    num_features = info_data.get('count')
+    full_extents = info_data.get("fullExtent", {})
+    if len(full_extents) == 0:
+        raise ValueError("The server does not have 'fullExtent'.")
+    minX = full_extents['xmin']
+    minY = full_extents['ymin']
+    maxX = full_extents['xmax']
+    maxY = full_extents['ymax']
+    return (max_record_count, num_features, (minX, minY, maxX, maxY))
+
 # Main function to query the server
 def query_all_data(base_url, layer_number, output_geojson_filepath, wait_time, grid_size, sudo=False, minX=None, minY=None, maxX=None, maxY=None):
     features = []
 
     # Get the max number of results that can be returned, so we can check if we ever hit it.
     # Also get the extents so the user doesn't have to specify them
-    info_url = base_url + "?f=json"
-    info_response = requests.get(info_url)
-    info_data = info_response.json()
-    max_record_count = info_data.get("maxRecordCount", "Not specified")
-    num_features = info_data.get('count')
+    max_record_count, num_features, bounds = get_server_info(base_url)
     if minX == None:
-        full_extents = info_data.get("fullExtent", {})
-        if len(full_extents) == 0:
-            raise ValueError("The server does not have 'fullExtent'.")
-        minX = full_extents['xmin']
-        minY = full_extents['ymin']
-        maxX = full_extents['xmax']
-        maxY = full_extents['ymax']
+        minX, minY, maxX, maxY = bounds
 
     url = base_url + '/' + str(layer_number) + '/query'
 
@@ -133,20 +148,59 @@ def main():
     parser.add_argument("-o", "--output-geojson-filepath", required=True, help="Output geojson filepath")
     parser.add_argument("--dont-convert-to-shapefile", action='store_true', help="Don't convert geojson to shp.zip with the same name.")
     parser.add_argument("--sudo", action='store_true', help="You will be prompted to pass this to run a very large set of requests.")
+    parser.add_argument("--subdivide", required=False, type=int, help="Split the job into an n x n square of jobs, resulting in n^2 files.")
 
     args = parser.parse_args()
 
-    if not (args.min_x or args.min_y or args.max_x or args.max_y):
-        # If none are specified, detect the extents
-        query_all_data(args.base_url, args.layer_number, args.output_geojson_filepath, args.wait_time, args.grid_size if args.grid_size else 0.001, sudo=args.sudo)
+    # If subdividing, use subprocess to run this script a bunch of times
+    if args.subdivide:
+        if not (args.min_x or args.min_y or args.max_x or args.max_y):
+            # If none are specified, detect the extents
+            _, _, (minX, minY, maxX, maxY) = get_server_info(args.base_url)
+        else:
+            if not (args.min_x and args.min_y and args.max_x and args.max_y):
+                raise ValueError("Must specify all of min_x, min_y, max_x, max_y.")
+            minX, minY, maxX, maxY = (args.min_x, args.min_y, args.max_x, args.max_y)
+        square_size_x = (maxX - minX) / args.subdivide
+        square_size_y = (maxY - minY) / args.subdivide
+        grid_size = min((args.grid_size, square_size_x, square_size_y))
+        # Iterate over the grid
+        filename_index = 1
+        for i in range(args.subdivide):
+            square_min_x = number_to_command_line_arg(minX + i * square_size_x)
+            square_max_x = number_to_command_line_arg(minX + (i + 1) * square_size_x)
+            for j in range(args.subdivide):
+                print("Running job %s %d/%d." % (THIS_SCRIPT_NAME, filename_index, args.subdivide * args.subdivide))
+                square_min_y = number_to_command_line_arg(minY + j * square_size_y)
+                square_max_y = number_to_command_line_arg(minY + (j + 1) * square_size_y)
+                current_geojson_filepath = args.output_geojson_filepath[:-len(".geojson")] + str(filename_index) + ".geojson"
+                command_list = ['python3', THIS_SCRIPT_NAME, "--base-url", args.base_url, "-n", number_to_command_line_arg(args.layer_number),\
+                        "-X", square_min_x, "-Y", square_min_y, "-U", square_max_x, "-V", square_max_y,\
+                        "--wait-time", number_to_command_line_arg(args.wait_time),\
+                        "--grid-size", number_to_command_line_arg(grid_size), "-o", current_geojson_filepath]
+                if args.sudo:
+                    command_list.append("--sudo")
+                if args.dont_convert_to_shapefile:
+                    command_list.append("--dont-convert-to-shapefile")
+                print(command_list)
+                p = subprocess.Popen(command_list, shell=True)
+                p.communicate()
+                filename_index += 1
     else:
-        if not (args.min_x and args.min_y and args.max_x and args.max_y):
-            raise ValueError("Must specify all of min_x, min_y, max_x, max_y.")
-        query_all_data(args.base_url, args.layer_number, args.output_geojson_filepath, args.wait_time, args.grid_size if args.grid_size else 0.001, args.sudo, args.min_x, args.min_y, args.max_x, args.max_y)
+        # If not subdiving, run this script's functionality once
+        if not (args.min_x or args.min_y or args.max_x or args.max_y):
+            # If none are specified, detect the extents
+            query_all_data(args.base_url, args.layer_number, args.output_geojson_filepath, args.wait_time,\
+                    args.grid_size if args.grid_size else 0.001, sudo=args.sudo)
+        else:
+            if not (args.min_x and args.min_y and args.max_x and args.max_y):
+                raise ValueError("Must specify all of min_x, min_y, max_x, max_y.")
+            query_all_data(args.base_url, args.layer_number, args.output_geojson_filepath, args.wait_time,\
+                    args.grid_size if args.grid_size else 0.001, args.sudo, args.min_x, args.min_y, args.max_x, args.max_y)
 
-    if not args.dont_convert_to_shapefile:
-        p = subprocess.Popen(['python3', CONVERT_TO_SHAPEFILE_NAME, "-i", args.output_geojson_filepath], shell=True)
-        p.communicate()
+        if not args.dont_convert_to_shapefile:
+            p = subprocess.Popen(['python3', CONVERT_TO_SHAPEFILE_NAME, "-i", args.output_geojson_filepath], shell=True)
+            p.communicate()
 
 if __name__ == "__main__":
     main()
